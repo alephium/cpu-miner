f811b4f3191e2f41f971fb17648a1f3af452a944
Cheng Wang <wachmc@gmail.com>
2021-09-20 21:16:07 +0200

    Fix constants for Linux. Credits to killerwhile

--- src/constants.h
+++ src/constants.h
@@ -1,16 +1,9 @@
 #ifndef ALEPHIUM_CONSTANTS_H
 #define ALEPHIUM_CONSTANTS_H
 
-#include <stdio.h>
-#include <stdint.h>
-#include <assert.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdlib.h>
-
-const uint32_t group_nums = 4;
-const uint32_t chain_nums = group_nums * group_nums;
-const uint32_t parallel_mining_works = chain_nums;
-const uint32_t mining_steps = 20000;
+#define group_nums 4
+#define chain_nums 16
+#define parallel_mining_works 4
+#define mining_steps 20000
 
 #endif // ALEPHIUM_CONSTANTS_H
